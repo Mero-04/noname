b29d64feb6e1af0d914bce6c6f50a2148dde13fe
Mero-04 <meromuhammedov44@gmail.com>
2023-09-24 15:21:45 +0300

    banner apii

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT;
 const morgan = require('morgan')
 const cors = require("cors");
 const sequelize = require('./data/db');
-const helmet = require("helmet");
+// const helmet = require("helmet");
 // const apilimiter = require("./middlewares/rateLimit");
 // app.use("/api", apilimiter)
 
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(cors());
-app.use(helmet());
+// app.use(helmet());
 app.use(morgan('tiny'));
 app.use('/api', express.static('public'))
 
